32456f4996ebc493ffea9d53c50e67ed81318821
Kinzokuzu <sauljcervantes2@gmail.com>
2023-09-20 13:58:39 -0700

    getBinaryList() now based on # of variables, updated tests to reflect

--- mizer.py
+++ mizer.py
@@ -1,5 +1,4 @@
-import sys
-
+# gets variables from a passed function in the form F([variables]) = sum([terms])
 def getVariables(function: str) -> list[str]:
     v_list = []
     # TODO: implement checks for variables
@@ -8,6 +7,7 @@ def getVariables(function: str) -> list[str]:
 
     return v_list
 
+# gets term subscripts from a passed function who's terms are in a list (see above function form)
 def getTermSubscripts(function: str) -> list[int]:
     term_subs = []
     # TODO: implement checks for terms & subscripts
@@ -16,23 +16,31 @@ def getTermSubscripts(function: str) -> list[int]:
 
     return term_subs
 
-def getBinaryList(num_list: list[int]) -> list[str]:
+# returns binary representaion of intergers passed from num_list
+def getBinaryList(num_list: list[int], bits: int) -> list[str]:
     binary_list = []
 
+    curr_binary_num = ""
     for n in num_list:
-        binary_list.append(bin(n))
+        # ignore "0b"
+        curr_binary_num = bin(n)[2::]
+        # preppend the appropriate # of '0's given bits
+        binary_list.append('0'*(bits - len(curr_binary_num)) + curr_binary_num)
+
 
     return binary_list
 
+# returns the appropriate minterm given a list of variables and the term subscript in binary
 def getMinterm(var_list: list[str], binary_subscript: str) -> str:
     minterm = ""
 
-    r_binary_num= binary_subscript[::-1]
+    r_binary_num = binary_subscript[::-1]
     end_of_binary_num = False
     for i in range(len(var_list)):
+        # ingore all characters after the 'b' in binary number string
         if not end_of_binary_num and r_binary_num[i] == 'b':
             end_of_binary_num = True
-
+        # apppend appropriate true or complimented term
         if not end_of_binary_num and r_binary_num[i] == '1':
             minterm += var_list[i]
         else:
@@ -40,6 +48,7 @@ def getMinterm(var_list: list[str], binary_subscript: str) -> str:
 
     return minterm
 
+# returns the standard representation of a sum of products function given its variables and minterm subscripts
 def getStandardRepresentation(var_list: list[str], subscript_list: list[int]) -> str:
     variable_count = len(var_list)
     function = "F("
@@ -57,7 +66,7 @@ def getStandardRepresentation(var_list: list[str], subscript_list: list[int]) ->
     function += " = "
 
     # append terms
-    binary_list = getBinaryList(subscript_list)
+    binary_list = getBinaryList(subscript_list, variable_count)
     for i in range(len(subscript_list)):
         function += getMinterm(var_list, binary_list[i])
 
--- test.py
+++ test.py
@@ -1,58 +1,14 @@
-import mizer
+#!/usr/bin/env python3
 
-import sys, os
+import functionTests as ft
+import sys
 
-# disable
-def disablePrint():
-    sys.stdout = open(os.devnull, 'w')
+if len(sys.argv) > 1:
+    if sys.argv[1] == "all":
+        ft.test_getVariables()
+        ft.test_getTermSubscripts()
+        ft.test_getBinaryList()
+        ft.test_getMinterm()
+        ft.test_getStandardRepresentation()
 
-# restore
-def enablePrint():
-    sys.stdout = sys.__stdout__
-
-def getVariables_test1():
-    test_var_str = "ABCD"
-    test_var_list = ["A","B","C","D"]
-    test_var_count = 4
-
-    var_list, var_count = mizer.getVariables(test_var_str)
-
-    print("getVariables_test1")
-    result = True
-    if var_list != test_var_list:
-        print("FAILED: var_list != test_var_list")
-        result = False
-
-    if var_count != test_var_count:
-        print("FAILED: var_count != test_var_count")
-        result = False
-
-    if result == True:
-        print("PASSED")
-
-def integerToBinaryString_test1():
-    test_int = 7
-    test_bits = 4
-    test_binary_num = "0111"
-
-    if mizer.integerToBinaryString(test_int, test_bits) != test_binary_num:
-        print("Failed: ")
-
-def getStandardRepresentation_test1():
-    test_vars, test_var_count = mizer.getVariables("ABCD")
-    test_tgt_func = "0,1,2,4,6,8,9,"
-
-    # expected_result = "A'B'C'"
-    standard_representation = mizer.getStandardRepresentation(test_vars, test_tgt_func)
-    mizer.printFunction(standard_representation)
-
-def getTermSubscripts_test1():
-    test_tgt_func = "0,1,2,11,123,"
-    
-    print(mizer.getTermSubscripts(test_tgt_func))
-
-# test calls
-getTermSubscripts_test1()
-getVariables_test1()
-integerToBinaryString_test1()
-getStandardRepresentation_test1()
+print("TESTING COMPLETE")
