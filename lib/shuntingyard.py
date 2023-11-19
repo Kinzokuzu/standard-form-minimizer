01c6d494e1d0b61434ab86783163cfee9b5d6898
Kinzokuzu <sauljcervantes2@gmail.com>
2023-11-18 23:25:05 -0800

    file renames

---
+++ lib/shuntingyard.py
@@ -0,0 +1,2 @@
+def random_func():
+    print("called random_func() form shuntingyard.py")
---
+++ tests/test_shuntingyard.py
@@ -0,0 +1,9 @@
+import unittest
+from lib import shuntingyard as sy
+
+class TestShuntingYardFunctions(unittest.TestCase):
+    def test_random_func(self):
+        sy.random_func()
+
+if __name__ == '__main__':
+    unittest.main()
