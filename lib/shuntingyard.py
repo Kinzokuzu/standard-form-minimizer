02e67ae38e0329e20a38f3e8134f6bce137036c8
Kinzokuzu <sauljcervantes2@gmail.com>
2023-11-20 18:30:29 -0800

    wrote test for shuntingyard.get_inputs_outputs()

--- lib/shuntingyard.py
+++ lib/shuntingyard.py
@@ -1,2 +1,24 @@
-def random_func():
-    print("inside random function")
+
+def get_inputs_outputs(lhs: str):
+    inputs = []
+    outputs = []
+
+    in_parentheses = False
+
+    for token in lhs:
+        match token:
+            case ' ': # ignore whitespace
+                pass
+            case ',': # ignore delimeters
+                pass
+            case '(': # entering parentheses
+                in_parentheses = True
+            case ')': # exiting parentheses
+                in_parentheses = False
+            case _:
+                if in_parentheses: # handle inputs (variables)
+                    inputs.append(token)
+                else: # handle outputs (function name)
+                    outputs.append(token) 
+    
+    return inputs, outputs
--- tests/test_shuntingyard.py
+++ tests/test_shuntingyard.py
@@ -2,8 +2,16 @@ import unittest
 from lib import shuntingyard as sy
 
 class TestShuntingYardFunctions(unittest.TestCase):
-    def test_random_func(self):
-        sy.random_func()
+    def test_get_inputs_outputs(self):
+        test_string = "F(A,B,C)"
+
+        expected_inputs = ['A','B','C']
+        expected_outputs = ['F']
+
+        inputs, outputs = sy.get_inputs_outputs(test_string)
+
+        self.assertEqual(inputs, expected_inputs)
+        self.assertEqual(outputs, expected_outputs)
 
 if __name__ == '__main__':
     main()
