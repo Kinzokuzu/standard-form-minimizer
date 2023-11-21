b77e106677a605ce8b40adbe4d6daba936dea857
Kinzokuzu <sauljcervantes2@gmail.com>
2023-11-20 19:29:13 -0800

    wrote tests for shuntingyard.shunting_yard()

--- lib/shuntingyard.py
+++ lib/shuntingyard.py
@@ -1,3 +1,34 @@
+# helper functions for shunting_yard
+def negate_variable(var: str, neg: bool):
+    if neg: return "!" + var
+    else: return var
+# end helper functions for shunting_yard
+
+
+def shunting_yard(rhs: str):
+    rpn = []        # reverse polish notation      
+    op_stack = []   # operator stack
+
+    negate_var = False
+
+    for i in range(len(rhs)):
+        if rhs[i] == ' ': # ignore whitespace
+            pass
+        elif rhs[i] == '+': # 'or' operator
+            op_stack.append('or')
+        elif rhs[i] == '!': # negation with '!'
+            negate_var = True
+        else:
+            if rhs[i].isalpha():
+                # look for negation with apostrophe
+                if rhs[i+1] == '\'':
+                    negate_var = True
+                
+                rpn.append(negate_variable(rhs[i], negate_var))
+
+
+    return rpn
+
 
 def get_inputs_outputs(lhs: str):
     inputs = []
--- tests/test_shuntingyard.py
+++ tests/test_shuntingyard.py
@@ -2,6 +2,37 @@ import unittest
 from lib import shuntingyard as sy
 
 class TestShuntingYardFunctions(unittest.TestCase):
+    def test_helper_negate_variable(self):
+        expected_true = "A"
+        expected_negation = "!A"
+
+        true_var = sy.negate_variable('A', False)
+        negated_var = sy.negate_variable('A', True)
+
+        self.assertEqual(true_var, expected_true)
+        self.assertEqual(negated_var, expected_negation)
+
+
+    def test_shunting_yard_easy(self):
+        test_string = "AB'"
+        
+        expected_rpn = ["A","!B","and"]
+
+        rpn = sy.shunting_yard(test_string)
+
+        self.assertEqual(rpn, expected_rpn)
+
+
+    def test_shunting_yard_hard(self):
+        test_string = "AB' + A'B"
+        
+        expected_rpn = ["A","!B","!A","B","and","and","or"]
+
+        rpn = sy.shunting_yard(test_string)
+
+        self.assertEqual(rpn, expected_rpn)
+
+
     def test_get_inputs_outputs(self):
         test_string = "F(A,B,C)"
 
