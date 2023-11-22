0346e6a4408c4aaaa273a1eed5f9551b0cdb6f7d
Kinzo <sauljcervantes2@gmail.com>
2023-11-22 00:24:13 -0800

    began writing ttable and tests

--- tests/test_ttable.py
+++ tests/test_ttable.py
@@ -2,8 +2,19 @@ import unittest
 from lib import ttable as tt
 
 class TestTTableFunctions(unittest.TestCase):
-    def test_random_func(self):
-        pass
+    def test_generate_table_easy(self):
+        test_inputs = ['A','B']
+        test_outputs = ['F']
+
+        expected_table = [['A','B','|','F'],
+                          ['0','0','|','0'],
+                          ['0','1','|','0'],
+                          ['1','0','|','0'],
+                          ['1','1','|','0'],]
+
+        generated_table = tt.generate_table(test_inputs, test_outputs)
+
+        self.assertEqual(generated_table, expected_table)
 
 if __name__ == '__main__':
-    pass
+    main()
