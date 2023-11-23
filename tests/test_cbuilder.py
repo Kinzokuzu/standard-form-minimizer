32ff704c5e792c5abc1d3ee385f19e248c07c5ed
Kinzo <sauljcervantes2@gmail.com>
2023-11-23 13:19:44 -0800

    fix bugs related to cbuilder.py and string formating - see test_cbuilder.py

--- tests/test_cbuilder.py
+++ tests/test_cbuilder.py
@@ -1,9 +1,39 @@
 import unittest
+from lib import boolfunc as bf
 from lib import cbuilder as cb 
 
 class TestCBuilderFunctions(unittest.TestCase):
-    def test_random_func(self):
-        pass
+    def test_write_description(self):
+        test_string = "AB\'"
+
+        expected_string = "// implementation of AB\'\n"
+
+        result_string = cb.write_description(test_string)
+
+        self.assertEqual(result_string, expected_string)
+
+
+    def test_write_inputs(self):
+        test_inputs = ['A','B']
+
+        expected_string = """// inputs\n
+                             input A;\n
+                             input B;\n"""
+
+        result_string = cb.write_inputs(test_inputs)
+
+        self.assertEqual(result_string, expected_string)
+
+    def test_write_outputs(self):
+        test_outputs = ['F']
+
+        expected_string = """// outputs\n
+                             output F;\n"""
+
+        result_string = cb.write_outputs(test_outputs)
+
+        self.assertEqual(result_string, expected_string)
+
 
 if __name__ == '__main__':
     pass
