83beffc8069778b6b471b547e88f6dd4d7c12f23
Kinzokuzu <sauljcervantes2@gmail.com>
2023-11-27 09:42:50 -0800

    began cli branch

---
+++ lib/main.py
@@ -0,0 +1,12 @@
+from lib import boolfunc as bf
+from lib import cbuilder as cb
+from lib import quinemcclusky as qm
+from lib import shuntingyard as sh # sy was too similar to sys
+from lib import ttable as tt
+
+import sys
+
+usage_message = "usage: vcb -[options] [function]"
+
+def main():
+    pass
